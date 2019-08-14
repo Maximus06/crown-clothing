aa257a9f584ae315a687f863e0f1f279c60f94c6
Maximus06 <o.maj@free.fr>
2019-08-14 16:10:47 +0200

    add a header component to dispose a general menu on each page

--- src/App.js
+++ src/App.js
@@ -4,29 +4,15 @@ import { Switch, Route, Link } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
-const Womens = () => (
-  <div>
-    <Link to="/">Back to home</Link>
-    <h1>VOUS LES FEMMES</h1>
-  </div>
-);
-
-const Topics = () => (
-  <div>
-    <Link to="/">Back to home</Link>
-    <h1>TOPICS</h1>
-  </div>
-);
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-        <Route path="/shop/womens" component={Womens} />
-        <Route path="/topics" component={Topics} />
       </Switch>
     </div>
   );
