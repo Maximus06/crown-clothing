49bbd03c3277ed77543ff8424304e93e327a81a5
Maximus06 <o.maj@free.fr>
2019-08-16 11:59:29 +0200

    Add form-input and custom-button component for login feature

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route, Link } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import LoginPage from "./pages/login/login.component";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/login" component={LoginPage} />
       </Switch>
     </div>
   );
