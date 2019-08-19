2c928ac4d1e6ce322e902981058939b631704dd0
Maximus06 <o.maj@free.fr>
2019-08-20 00:58:42 +0200

    finish the sign in feature

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./sign-in.styles.scss";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 import CustomButton from "../custom-button/custom-button.component";
 import FormInput from "../form-input/form-input.component";
 
@@ -15,10 +15,18 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      // clear the form
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log("error : ", error.message);
+    }
   };
 
   handleChange = event => {
