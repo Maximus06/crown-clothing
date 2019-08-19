066b5adbd2d9cc9a6b8607b5cb64f6d6876fdb52
Maximus06 <o.maj@free.fr>
2019-08-19 20:19:30 +0200

    implemented firebase utils with ability to store authenticated users into firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,38 @@ const config = {
   appId: "1:205021347739:web:d8720a07b59557e5"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  // userAuth est l'objet renvoyé par l'api google
+  if (!userAuth) return;
+
+  // get a reference on the document user
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  // get the data
+  const snapShot = await userRef.get();
+
+  // the user does not exist in the database yet, we are going to create him.
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating message :", error.message);
+    }
+  }
+
+  return userRef;
+
+  console.log(snapShot);
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/login/login.component.jsx
+++ src/pages/login/login.component.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
+
+import "./login.styles.scss";
 
 const LoginPage = () => (
-  <div className="sign-in-and-sign-up">
+  <div className="login">
     <SignIn />
+    <SignUp />
   </div>
 );
 
